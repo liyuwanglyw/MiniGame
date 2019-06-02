10cfc7a3bad0c6d0e0c3e9a9a1f204f7e3fcabc2
Xccelerate0 <2501290970@qq.com>
2019-06-02 14:37:57 +0800

    movementproblem

---
+++ Assets/Scenes/Levelchoose/Cameramovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameramovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    private Vector3 offset = new Vector3(0,0,10);
+
+    void Start()
+    {
+        //设置相对偏移
+        offset = target.position - this.transform.position;
+    }
+
+    void Update()
+    {
+        this.transform.position = target.position - offset;
+    }
+}
