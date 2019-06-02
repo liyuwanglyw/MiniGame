1cb329da6da2fcd6207abeac883919dbbab0c027
liyuwanglyw <921766678@qq.com>
2019-06-02 16:02:08 +0800

    1

---
+++ Assets/Scripts/MapControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapControl : MonoBehaviour
+{
+    public static BaseModule[,] modules;
+    public static int m,n;
+    private void Awake()
+    {
+    }
+}
---
+++ Assets/Scripts/TPipeModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPipeModule : BaseModule
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OutputState()
+    {
+        base.OutputState();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+}
