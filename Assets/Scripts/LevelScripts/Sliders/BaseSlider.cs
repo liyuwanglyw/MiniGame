b5ae07ca23e225e2285e1c7055b2748e2f262837
liyuwanglyw <921766678@qq.com>
2019-06-07 12:27:09 +0800

    1

---
+++ Assets/Scripts/LevelScripts/Sliders/BaseSlider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BaseSlider : MonoBehaviour
+{
+    public float flow_speed=200;
+    protected Slider[] sliders;
+    protected Image[] fill_img;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sliders = GetComponentsInChildren<Slider>();
+        fill_img = new Image[sliders.Length];
+        for(int i=0;i<sliders.Length;i++)
+        {
+            fill_img[i] = sliders[i].transform.GetChild(0).Find("Fill").GetComponent<Image>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FillPipe(Color[] colors)
+    {
+        StartCoroutine(StartFlow(colors));
+    }
+
+    public virtual IEnumerator StartFlow(Color[] colors)
+    {
+        yield return null;
+    }
+
+    public void CleanPipe()
+    {
+        for(int i=0;i<sliders.Length;i++)
+        {
+            sliders[i].value = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelScripts/Sliders/TPipeSlider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPipeSlider : BaseSlider
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override IEnumerator StartFlow(Color[] colors)
+    {
+        int len = (sliders.Length < colors.Length ? sliders.Length : colors.Length);
+        for(int i=0;i<len;i++)
+        {
+            fill_img[i].color = colors[i];
+        }
+
+        while(true)
+        {
+            sliders[0].value += flow_speed * Time.deltaTime;
+            if(sliders[0].value>99)
+            {
+                break;
+            }
+            yield return null; 
+        }
+        while (true)
+        {
+            sliders[1].value += flow_speed * Time.deltaTime;
+            sliders[2].value += flow_speed * Time.deltaTime;
+            if (sliders[1].value > 99)
+            {
+                break;
+            }
+            yield return null;
+        }
+
+    }
+}
