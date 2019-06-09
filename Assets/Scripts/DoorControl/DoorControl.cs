0326400979c9b816fb287ad2cac44e2e44241e69
LocoOvO <44262060+LocoOvO@users.noreply.github.com>
2019-06-10 07:56:02 +0800

    门控制更新

--- Assets/Scripts/DoorControl/DoorControl.cs
+++ Assets/Scripts/DoorControl/DoorControl.cs
@@ -6,16 +6,20 @@ public class DoorControl : MonoBehaviour
 {
     public bool Key = false;
     public GameObject[] obj;
-    public int n = 0;
+    public int n = 0;  //每次会显示的镜头
     private int i = 0;
+
     public  Vector3 Character_Pos = new Vector3 (0,0,0);
     public float Dis;
+
     private GameObject Character;
+
+    private  GameObject[] door;
+    public GameObject nearestdoor; //最近一个需要开关的门
     // Start is called before the first frame update
     void Start()
     {
-
-
+        
 
 
         obj[0].SetActive(true);
@@ -28,21 +32,23 @@ public class DoorControl : MonoBehaviour
         obj[7].SetActive(false);
         obj[8].SetActive(false);
 
+        door = GameObject.FindGameObjectsWithTag("door");
         Character = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        FindNearDoor();
         Dis = Vector3.Distance(this.transform.position, Character.transform.position);
         if (Dis   < 1.5f  && Input .GetKeyDown (KeyCode.Space  ) && Key)
         {
             Debug.Log("距离足够");
-            DoorOpen();
+            ChangeCamera ();
         }
     }
 
-    private void DoorOpen()
+    private void ChangeCamera()
     {
         i = 0;
        
@@ -53,6 +59,25 @@ public class DoorControl : MonoBehaviour
         }
             obj[n].SetActive(true);
         Character.transform.position = Character_Pos;
+        
+    }
+
+    private void FindNearDoor()
+    {
+        i = 0;
+        nearestdoor = door[0];
+        while (i<18)
+        {
+            if (Vector3.Distance(door[i].transform.position, Character.transform.position) < Vector3.Distance(nearestdoor.transform.position, Character.transform.position))
+            {
+                nearestdoor = door[i];
+            }
+            i = i + 1;
+        }
+    }
+
+    private void CloseDoor()
+    {
         
     }
 }
