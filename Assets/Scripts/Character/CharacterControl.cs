684a56d5c9fb4f9943209fbb07dae68eda3a532b
LocoOvO <44262060+LocoOvO@users.noreply.github.com>
2019-06-10 15:20:26 +0800

    Robin

---
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    private GameObject Robin = GameObject.Find("Robin");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+    
+    private void Walk()
+    {
+        Animation anim = Robin.GetComponent<Animation >();
+        AnimationClip walk = anim.GetClip("Run");
+
+        anim.clip = walk;
+        anim.Play();
+    }
+}
