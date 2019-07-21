db26df0e67fda5a026640f72db4edb6c2461f16a
AppDoraemon <694458125@qq.com>
2019-07-21 21:23:45 +0800

    关卡引导

---
+++ Assets/Scripts/Managers/GuideManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideManager : MonoBehaviour
+{
+    public GameObject[] Texts;
+    public GameObject[] HighLight;
+    private int pointer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void PointerDown()
+    {
+        Texts[pointer].SetActive(false);
+        HighLight[pointer].SetActive(false);
+        pointer++;
+
+        if (pointer != Texts.Length)
+        {
+            Texts[pointer].SetActive(true);
+            HighLight[pointer].SetActive(true);
+        }
+        else
+            Destroy(gameObject);
+
+    }
+
+}
