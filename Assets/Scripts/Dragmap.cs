753cbdd8b6f15de0aa3b9eb4e007907a62ddd5ca
Xccelerate0 <2501290970@qq.com>
2019-07-21 18:18:38 +0800

    1

--- Assets/Scripts/Dragmap.cs
+++ Assets/Scripts/Dragmap.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 
 public class Dragmap : MonoBehaviour, IPointerClickHandler
 {
-    public GameObject mapitem;
-    public int itemdirection;
-    public int itemcolor;
+    public leveleditor editor;
+    public GameObject item;
+    public GameObject panel;
+    public int type;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        editor = GameObject.Find("Canvas").GetComponent<leveleditor>();
+        Debug.Log(editor.r);
     }
 
     // Update is called once per frame
@@ -21,18 +24,35 @@ public class Dragmap : MonoBehaviour, IPointerClickHandler
         
     }
 
-    public void changedirection(int direction)
-    {
-        itemdirection = direction;
-    }
-    public void changecolor(string direction)
-    {
-        int d = int.Parse(direction);
-        itemdirection = d;
-    }
-
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
-
+        if(type == 0)
+        {
+            GameObject citem = Instantiate(item);
+            citem.transform.parent = panel.transform;
+        }
+        if(type == 1)
+        {
+            GameObject citem = Instantiate(item);
+            citem.GetComponent<BaseModule>().gen_color.isValid = true;
+            citem.GetComponent<BaseModule>().gen_color.r = editor.r;
+            citem.GetComponent<BaseModule>().gen_color.g = editor.g;
+            citem.GetComponent<BaseModule>().gen_color.b = editor.b;
+            citem.GetComponent<BaseModule>().direct = editor.direction;
+            citem.GetComponent<RectTransform>().rotation = Quaternion.Euler(0.0f, 0.0f, (editor.direction+3) * 90f);
+            citem.transform.parent = panel.transform;
+        }
+        if (type == 2)
+        {
+            GameObject citem = Instantiate(item);
+            citem.GetComponent<BaseModule>().rev_color.isValid = true;
+            citem.GetComponent<BaseModule>().rev_color.r = editor.r;
+            citem.GetComponent<BaseModule>().rev_color.g = editor.g;
+            citem.GetComponent<BaseModule>().rev_color.b = editor.b;
+            citem.GetComponent<BaseModule>().direct = editor.direction;
+            citem.GetComponent<RectTransform>().rotation = Quaternion.Euler(0.0f, 0.0f, (editor.direction + 1) * 90f);
+            citem.transform.parent = panel.transform;
+        }
+  
     }
 }
